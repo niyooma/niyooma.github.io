3009c1bf649f87017b32335638863b7f9041f8cc
niyooma <dunickj2@126.com>
2014-08-21 16:15:07 +0800

    11

--- sidebar_js/sidebar2.js
+++ sidebar_js/sidebar2.js
@@ -1,6 +1,6 @@
-document.writeln('<li id="text-3" class="widget widget_text"><h4>近期评论</h4>	');
+document.writeln('<li id="text-3" class="widget widget_text"><h4></h4>	');
 document.writeln('<div class="textwidget">');
-document.writeln('<div id="uyan_newcmt_unit" style="width: 98%; border:1px solid #f0f0f0; border-radius: 5px;"></div>');
+document.writeln('<div id="uyan_newcmt_unit" style="width: 96%; border:1px solid #f0f0f0; border-radius: 5px;"></div>');
 document.writeln('<script type="text/javascript" src="http://v2.uyan.cc/code/uyan.js?uid=1953656"></script>');
 document.writeln('</div>');
 document.writeln('</li>');

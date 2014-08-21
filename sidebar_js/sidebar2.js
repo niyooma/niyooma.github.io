bf985591adffb517a97b968ffe829f88a3c826dc
niyooma <dunickj2@126.com>
2014-08-21 16:16:17 +0800

    modify sidebar2.js

--- sidebar_js/sidebar2.js
+++ sidebar_js/sidebar2.js
@@ -1,4 +1,3 @@
-document.writeln('<li id="text-3" class="widget widget_text"><h4></h4>	');
 document.writeln('<div class="textwidget">');
 document.writeln('<div id="uyan_newcmt_unit" style="width: 96%; border:1px solid #f0f0f0; border-radius: 5px;"></div>');
 document.writeln('<script type="text/javascript" src="http://v2.uyan.cc/code/uyan.js?uid=1953656"></script>');

369b317d59dd909631f94c03b3582150650a0a3d
niyooma <dunickj2@126.com>
2014-08-21 15:34:54 +0800

    rewrite archives.html

---
+++ post_list.js
@@ -0,0 +1,73 @@
+var list_cnt =14;
+﻿var postarch=new Array();
+ 
+for(var i=0;i<list_cnt;i++){
+ 
+postarch[i]=new Array(); 
+
+for(var j=0;j<2;j++)
+{
+ postarch[i][j]=1;
+ }
+ 
+}
+ 
+
+//赋值
+
+postarch[0][0]="世界，你好";
+ 
+postarch[0][1]="articles/p001.html";
+ 
+postarch[1][0]="试用wordpress的iphone app"; 
+
+postarch[1][1]="articles/p002.html";
+
+postarch[2][0]="普通立体声如何Up-mix到5.1声道:弯路 1"; 
+
+postarch[2][1]="articles/p003.html";
+
+postarch[3][0]="才想起来晚上又有爸爸去哪儿了"; 
+
+postarch[3][1]="articles/p004.html";
+
+postarch[4][0]="一直进步的小胖妞"; 
+
+postarch[4][1]="articles/p005.html";
+
+postarch[5][0]="没能去参与救援，献点血也算出份力吧"; 
+
+postarch[5][1]="articles/p006.html";
+
+postarch[6][0]="写实的热门电视剧们"; 
+
+postarch[6][1]="articles/p007.html";
+
+postarch[7][0]="问禅师之码农版"; 
+
+postarch[7][1]="articles/p008.html";
+
+postarch[8][0]="默哀"; 
+
+postarch[8][1]="articles/p009.html";
+
+postarch[9][0]="在侧边栏随机显示电影台词"; 
+
+postarch[9][1]="articles/p010.html";
+
+postarch[10][0]="所谓藏头诗"; 
+
+postarch[10][1]="articles/p011.html";
+
+postarch[11][0]="用声音充电?"; 
+
+postarch[11][1]="articles/p012.html";
+
+postarch[12][0]="效率最低的联系方式"; 
+
+postarch[12][1]="articles/p013.html";
+
+postarch[13][0]="静态博客正式上线"; 
+
+postarch[13][1]="articles/p014.html";
+
